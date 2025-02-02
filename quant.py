b4e0692c2a9dc2b6ad92df1eea3447cf1e758bf8
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2025-02-02 19:09:06 +0100

    Changed fail return type in all functions to False to avoid confusion

--- quant.py
+++ quant.py
@@ -51,98 +51,98 @@ def get_earnings(ticker : str, av_api_key : str):
 
 def liabilities_to_equity(balance_sheet : dict):
     """Calculates the Libailities-to-Equity ratio of a company whose balance sheet is 
-       passed as a dictionary-type argument. Returns -1 if it fails to calculate"""
+       passed as a dictionary-type argument. Returns False if it fails to calculate"""
     
     if balance_sheet["annualReports"][0]["totalLiabilities"] == "None" or balance_sheet["annualReports"][0]["totalShareholderEquity"] == "None" or balance_sheet["annualReports"][0]["totalShareholderEquity"] == "0":
-        return -1
+        return False
     
     return int(balance_sheet["annualReports"][0]["totalLiabilities"]) / int(balance_sheet["annualReports"][0]["totalShareholderEquity"])
 
 def liabilities_to_capital(balance_sheet : dict):
     """Calculates the Liabilities-to-Capital ratio of a company whose balance sheet is 
-       passed as a dictionary-type argument. Returns -1 if it fails to calculate"""
+       passed as a dictionary-type argument. Returns False if it fails to calculate"""
     
     if balance_sheet["annualReports"][0]["totalLiabilities"] == "None" or balance_sheet["annualReports"][0]["totalShareholderEquity"] == "None" or int(balance_sheet["annualReports"][0]["totalLiabilities"]) + int(balance_sheet["annualReports"][0]["totalShareholderEquity"]) == "0":
-        return -1
+        return False
     
     return int(balance_sheet["annualReports"][0]["totalLiabilities"]) / (int(balance_sheet["annualReports"][0]["totalLiabilities"]) + int(balance_sheet["annualReports"][0]["totalShareholderEquity"]))
 
 def assets_to_equity(balance_sheet : dict):
     """Calculates the Total Assets-to-Equity ratio of a company whose balance sheet is 
-       passed as a dictionary-type argument. Returns -1 if it fails to calculate"""
+       passed as a dictionary-type argument. Returns False if it fails to calculate"""
     
     if balance_sheet["annualReports"][0]["totalAssets"] == "None" or balance_sheet["annualReports"][0]["totalShareholderEquity"] == "None" or balance_sheet["annualReports"][0]["totalShareholderEquity"] == "0":
-        return -1
+        return False
     
     return int(balance_sheet["annualReports"][0]["totalAssets"]) / int(balance_sheet["annualReports"][0]["totalShareholderEquity"])
 
 def debt_to_ebitda(balance_sheet : dict, income_statement : dict):
     """Calculates the Total Debt-to-EBITDA ratio of a company whose balance sheet is 
-    passed as a dictionary-type argument. Returns -1 if it fails to calculate"""
+    passed as a dictionary-type argument. Returns False if it fails to calculate"""
 
     if balance_sheet["annualReports"][0]["totalLiabilities"] == "None" or income_statement["annualReports"][0]["ebitda"] == "None" or income_statement["annualReports"][0]["ebitda"] == "0":
-            return -1
+            return False
     
     return int(balance_sheet["annualReports"][0]["totalLiabilities"]) / int(income_statement["annualReports"][0]["ebitda"])
 
 def quick_ratio(balance_sheet : dict):
     """Calculates the quick ratio of a company whose balance sheet is 
-    passed as a dictionary-type argument. Returns -1 if it fails to calculate"""
+    passed as a dictionary-type argument. Returns False if it fails to calculate"""
     cash_plus_ce = balance_sheet["annualReports"][0]["cashAndCashEquivalentsAtCarryingValue"]
     ms = balance_sheet["annualReports"][0]["totalCurrentAssets"]
     nar = balance_sheet["annualReports"][0]["currentNetReceivables"]
     current_liabilities = balance_sheet["annualReports"][0]["totalCurrentLiabilities"]
     
     if cash_plus_ce == "None" or ms == "None" or nar == "None" or current_liabilities == "None" or current_liabilities == "0":
-        return -1
+        return False
     
     return (int(cash_plus_ce) + int(ms) + int(nar))/(int(current_liabilities))
 
 
 def current_ratio(balance_sheet : dict):
     """Calculates the current ratio of a company whose balance sheet is 
-    passed as a dictionary-type argument. Returns -1 if it fails to calculate"""
+    passed as a dictionary-type argument. Returns False if it fails to calculate"""
     if balance_sheet["annualReports"][0]["totalCurrentAssets"] == "None" or balance_sheet["annualReports"][0]["totalCurrentLiabilities"] == "None" or balance_sheet["annualReports"][0]["totalCurrentLiabilities"] == "0":
-        return -1
+        return False
     
     return int(balance_sheet["annualReports"][0]["totalCurrentAssets"]) / int(balance_sheet["annualReports"][0]["totalCurrentLiabilities"])
 
 def ten_yr_operating_expenses_growth(income_statement : dict):
     """Calculates the 10yr op. expenses growth of a company whose income statement is 
-    passed as a dictionary-type argument. Returns -1 if it fails to calculate"""
+    passed as a dictionary-type argument. Returns False if it fails to calculate"""
     if income_statement["annualReports"][0]["operatingExpenses"] == "None" or income_statement["annualReports"][9]["operatingExpenses"] == "None" or income_statement["annualReports"][9]["operatingExpenses"] == "0":
-        return -1
+        return False
     
     return ((int(income_statement["annualReports"][0]["operatingExpenses"]) - int(income_statement["annualReports"][9]["operatingExpenses"]))/(int(income_statement["annualReports"][9]["operatingExpenses"]))) * 100
 
 def ten_yr_assets_growth(balance_sheet : dict):
     """Calculates the 10yr assets growth of a company whose balance sheet is 
-    passed as a dictionary-type argument. Returns -1 if it fails to calculate"""
+    passed as a dictionary-type argument. Returns False if it fails to calculate"""
     if balance_sheet["annualReports"][0]["totalAssets"] == "None" or balance_sheet["annualReports"][9]["totalAssets"] == "None" or balance_sheet["annualReports"][9]["totalAssets"] == "0":
-        return -1
+        return False
     
     return ((int(balance_sheet["annualReports"][0]["totalAssets"]) - int(balance_sheet["annualReports"][9]["totalAssets"]))/(int(balance_sheet["annualReports"][9]["totalAssets"]))) * 100
 
 def ten_yr_liabilities_growth(balance_sheet : dict):
     """Calculates the 10yr liabilities growth of a company whose balance sheet is 
-    passed as a dictionary-type argument. Returns -1 if it fails to calculate"""
+    passed as a dictionary-type argument. Returns False if it fails to calculate"""
     if balance_sheet["annualReports"][0]["totalLiabilities"] == "None" or balance_sheet["annualReports"][9]["totalLiabilities"] == "None" or balance_sheet["annualReports"][9]["totalLiabilities"] == "0":
-        return -1
+        return False
     
     return ((int(balance_sheet["annualReports"][0]["totalLiabilities"]) - int(balance_sheet["annualReports"][9]["totalLiabilities"]))/(int(balance_sheet["annualReports"][9]["totalLiabilities"]))) * 100
 
 def ten_yr_cash_flow_growth(cash_flow_report : dict):
     """Calculates the 10yr liabilities growth of a company whose cash flow report is 
-    passed as a dictionary-type argument. Returns -1 if it fails to calculate"""
+    passed as a dictionary-type argument. Returns False if it fails to calculate"""
     if cash_flow_report["annualReports"][0]["operatingCashflow"] == "None" or cash_flow_report["annualReports"][9]["operatingCashflow"] == "None" or cash_flow_report["annualReports"][9]["operatingCashflow"] == "0":
-        return -1
+        return False
     
     return ((int(cash_flow_report["annualReports"][0]["operatingCashflow"]) - int(cash_flow_report["annualReports"][9]["operatingCashflow"]))/(int(cash_flow_report["annualReports"][9]["operatingCashflow"]))) * 100
 
 def ten_yr_share_count_growth(balance_sheet : dict):
     """Calculates the 10yr share count growth of a company whose balance sheet is 
-    passed as a dictionary-type argument. Returns -1 if it fails to calculate"""
+    passed as a dictionary-type argument. Returns False if it fails to calculate"""
     if balance_sheet["annualReports"][0]["commonStock"] == "None" or balance_sheet["annualReports"][9]["commonStock"] == "None" or balance_sheet["annualReports"][9]["commonStock"] == "0":
-        return -1
+        return False
     
     return ((int(balance_sheet["annualReports"][0]["commonStock"]) - int(balance_sheet["annualReports"][9]["commonStock"]))/(int(balance_sheet["annualReports"][9]["commonStock"]))) * 100
