48c195b9b41a4dbb5618a81f7143bbc6a6f23f23
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2025-02-05 08:47:11 +0100

    Refined prompts and added test cases

--- qualit.py
+++ qualit.py
@@ -13,17 +13,44 @@ genai.configure(api_key=gemeni_api_key)
 gemeni = genai.GenerativeModel("gemini-1.5-flash")
 
 def analyse_public_sentiment_company(ticker : str):
-    response = gemeni.generate_content(f"Analyse the general sentiment towards the company whose ticker is {ticker}. YOUR RESPONSE SHOULD ONLY INCLUDE A NUMBER BETWEEN 0 (= THE COMPANY IS VERY HATED) AND 1 (= THE COMPANY IS VERY LOVED)")
-    return response.text
+    response = gemeni.generate_content(f"Analyse the general sentiment towards the brand image of the company whose ticker is {ticker}. YOUR RESPONSE SHOULD ONLY INCLUDE A NUMBER BETWEEN 0 (= THE COMPANY IS VERY HATED) AND 1 (= THE COMPANY IS VERY LOVED). The score should follow a normal distribution curve. BE BRUTALLY HONEST AND OBJECTIVE. If the company has encountered large scandals or lawsuits in the past year this score should be low. Recent news should have a noticeable impact on this score.")
+    return response.text.strip()
 
 def analyse_public_sentiment_leadership(ticker : str):
-    response = gemeni.generate_content(f"Analyse the general sentiment towards the leadership of the company whose ticker is {ticker}. YOUR RESPONSE SHOULD ONLY INCLUDE A NUMBER BETWEEN 0 (= THE LEADERSHIP IS VERY HATED) AND 1 (= THE LEADERSHIP IS VERY LOVED)")
-    return response.text
+    response = gemeni.generate_content(f"Analyse the general sentiment towards the leadership (MOST especially the CEO) of the company whose ticker is {ticker}. YOUR RESPONSE SHOULD ONLY INCLUDE A NUMBER BETWEEN 0 (= THE LEADERSHIP IS VERY HATED) AND 1 (= THE LEADERSHIP IS VERY LOVED). The score should follow a normal distribution curve. BE BRUTALLY HONEST AND OBJECTIVE. If the CEO or other high management has been involved in many scandals this score should be low.")
+    return response.text.strip()
 
 def analyse_public_sentiment_sector(ticker : str):
-    response = gemeni.generate_content(f"Analyse the general sentiment towards the industry sector of the company whose ticker is {ticker}. YOUR RESPONSE SHOULD ONLY INCLUDE A NUMBER BETWEEN 0 (= THE SECTOR IS VERY HATED) AND 1 (= THE SECTOR IS VERY LOVED)")
-    return response.text
+    response = gemeni.generate_content(f"Analyse the general sentiment towards the industry sector of the company whose ticker is {ticker}. YOUR RESPONSE SHOULD ONLY INCLUDE A NUMBER BETWEEN 0 (= THE SECTOR IS VERY HATED) AND 1 (= THE SECTOR IS VERY LOVED). The score should follow a normal distribution curve. BE BRUTALLY HONEST AND OBJECTIVE. Analyse the sector independently from the company itself. If the sector is an enviornmentally-damaging one (like oil or natural gas), it should get a lower score.")
+    return response.text.strip()
 
 def analyse_esg_and_sustainability(ticker : str):
-    response = gemeni.generate_content(f"How enviornmentally and morally responsible is the company whose ticker is {ticker}? Are they good with ESG? YOUR RESPONSE SHOULD ONLY INCLUDE A NUMBER BETWEEN 0 (= THE COMPANY IS VERY IRRESPONSIBLE/IMMORAL) AND 1 (= THE COMPANY IS VERY RESPONSIBLE)")
-    return response.text
+    response = gemeni.generate_content(f"How enviornmentally and morally responsible is the company whose ticker is {ticker}? Are they good with ESG? YOUR RESPONSE SHOULD ONLY INCLUDE A NUMBER BETWEEN 0 (= THE COMPANY IS VERY IRRESPONSIBLE/IMMORAL) AND 1 (= THE COMPANY IS VERY RESPONSIBLE). The score should follow a normal distribution curve. BE BRUTALLY HONEST AND OBJECTIVE. If a company's operation sector is an enviornmentally-damaging one (like oil or natural gas), it should get a lower score.")
+    return response.text.strip()
+
+tick = "TSLA"
+print(f"\nCOMPANY TICKER: {tick}")
+print( "___________________________________________")
+print(f"Public sentiment towards company    | {analyse_public_sentiment_company(tick)} |")
+print(f"Public sentiment towards leadership | {analyse_public_sentiment_leadership(tick)} |")
+print(f"Public sentiment towards sector     | {analyse_public_sentiment_sector(tick)} |")
+print(f"ESG and sustainability score        | {analyse_esg_and_sustainability(tick)} |")
+print( "___________________________________________\n")
+
+tick = "MSFT"
+print(f"\nCOMPANY TICKER: {tick}")
+print( "___________________________________________")
+print(f"Public sentiment towards company    | {analyse_public_sentiment_company(tick)} |")
+print(f"Public sentiment towards leadership | {analyse_public_sentiment_leadership(tick)} |")
+print(f"Public sentiment towards sector     | {analyse_public_sentiment_sector(tick)} |")
+print(f"ESG and sustainability score        | {analyse_esg_and_sustainability(tick)} |")
+print( "___________________________________________\n")
+
+tick = "XOM"
+print(f"\nCOMPANY TICKER: {tick}")
+print( "___________________________________________")
+print(f"Public sentiment towards company    | {analyse_public_sentiment_company(tick)} |")
+print(f"Public sentiment towards leadership | {analyse_public_sentiment_leadership(tick)} |")
+print(f"Public sentiment towards sector     | {analyse_public_sentiment_sector(tick)} |")
+print(f"ESG and sustainability score        | {analyse_esg_and_sustainability(tick)} |")
+print( "___________________________________________\n")
