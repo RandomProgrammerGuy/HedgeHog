3ab75b0da4391b127e80bc41bc346cebd97f06b3
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2025-06-11 11:11:44 +0200

    Added 1, 2 and 5-year liabilities growth calculation functions

--- quant.py
+++ quant.py
@@ -12,17 +12,20 @@ def print_json(data : dict):
     """Prints JSON data in a more presentable way. Mainly used for Debugging."""
     print(json.dumps(data, indent=4))
 
+
 def alphavantage_api_request(ticker : str, function : str, av_api_key : str):
     """Auxiliary function that allows for easier AlphaVantage API calls"""
     req_rep =  requests.get(f"https://www.alphavantage.co/query?function={function}&symbol={ticker}&apikey={av_api_key}")
     check_api_limit_reached(req_rep)
     return req_rep
 
+
 def check_api_limit_reached(param : dict) :
     """Checks if the AlphaVantage API call limit is reached when the API is called"""
     if param == {'Information': 'Thank you for using Alpha Vantage! Our standard API rate limit is 25 requests per day. Please subscribe to any of the premium plans at https://www.alphavantage.co/premium/ to instantly remove all daily rate limits.'} :
         raise ConnectionRefusedError("ALPHA VANTAGE API REQUEST LIMIT REACHED. TRY AGAIN LATER, SUBSCRIBE TO PREMIUM OR CHANGE THE API KEY IN apikeys.py")
     
+
 
 def get_income_statement(ticker : str, av_api_key : str):
     """Retrieves the financial statement of a ticker. ticker has to be of type 
@@ -33,6 +36,7 @@ def get_income_statement(ticker : str, av_api_key : str):
     parsed_json = api_req_response.json()
     return parsed_json
 
+
 def get_balance_sheet(ticker : str, av_api_key : str):
     """Retrieves the balance sheet of a ticker. ticker has to be of type string.
        Function requires an AlphaVantage API key as an argument"""
@@ -41,6 +45,7 @@ def get_balance_sheet(ticker : str, av_api_key : str):
     check_api_limit_reached(api_req_response)
     parsed_json = api_req_response.json()
     return parsed_json
+
 
 def get_cash_flow(ticker : str, av_api_key : str):
     """Retrieves the cash flow of a ticker. ticker has to be of type string.
@@ -61,6 +66,7 @@ def get_earnings(ticker : str, av_api_key : str):
     parsed_json = api_req_response.json()
     return parsed_json
 
+
 def liabilities_to_equity(balance_sheet : dict):
     """Calculates the Libailities-to-Equity ratio of a company whose balance sheet is 
        passed as a dictionary-type argument. Returns False if it fails to calculate"""
@@ -71,6 +77,7 @@ def liabilities_to_equity(balance_sheet : dict):
         return False
     
     return (int(balance_sheet["annualReports"][1]["totalLiabilities"]) / int(balance_sheet["annualReports"][1]["totalShareholderEquity"]))
+
 
 def liabilities_to_capital(balance_sheet : dict):
     """Calculates the Liabilities-to-Capital ratio of a company whose balance sheet is 
@@ -83,6 +90,7 @@ def liabilities_to_capital(balance_sheet : dict):
     
     return (int(balance_sheet["annualReports"][1]["totalLiabilities"]) / (int(balance_sheet["annualReports"][1]["totalLiabilities"]) + int(balance_sheet["annualReports"][1]["totalShareholderEquity"])))
 
+
 def assets_to_equity(balance_sheet : dict):
     """Calculates the Total Assets-to-Equity ratio of a company whose balance sheet is 
        passed as a dictionary-type argument. Returns False if it fails to calculate"""
@@ -93,6 +101,7 @@ def assets_to_equity(balance_sheet : dict):
         return False
     
     return (int(balance_sheet["annualReports"][1]["totalAssets"]) / int(balance_sheet["annualReports"][1]["totalShareholderEquity"]))
+
 
 def debt_to_ebitda(balance_sheet : dict, income_statement : dict):
     """Calculates the Total Debt-to-EBITDA ratio of a company whose balance sheet is 
@@ -105,6 +114,7 @@ def debt_to_ebitda(balance_sheet : dict, income_statement : dict):
             return False
     
     return (int(balance_sheet["annualReports"][1]["totalLiabilities"]) / int(income_statement["annualReports"][1]["ebitda"]))
+
 
 def quick_ratio(balance_sheet : dict):
     """Calculates the quick ratio of a company whose balance sheet is 
@@ -134,6 +144,7 @@ def current_ratio(balance_sheet : dict):
     
     return (int(balance_sheet["annualReports"][1]["totalCurrentAssets"]) / int(balance_sheet["annualReports"][1]["totalCurrentLiabilities"]))
 
+
 def ten_yr_operating_expenses_growth(income_statement : dict):
     """Calculates the 10yr op. expenses growth of a company whose income statement is 
     passed as a dictionary-type argument. Returns False if it fails to calculate"""
@@ -147,6 +158,7 @@ def ten_yr_operating_expenses_growth(income_statement : dict):
         return False
     
     return ((int(income_statement["annualReports"][1]["operatingExpenses"]) - int(income_statement["annualReports"][10]["operatingExpenses"]))/(int(income_statement["annualReports"][10]["operatingExpenses"]))) * 100
+
 
 def ten_yr_assets_growth(balance_sheet : dict):
     """Calculates the 10yr assets growth of a company whose balance sheet is 
@@ -162,6 +174,52 @@ def ten_yr_assets_growth(balance_sheet : dict):
     
     return ((int(balance_sheet["annualReports"][1]["totalAssets"]) - int(balance_sheet["annualReports"][10]["totalAssets"]))/(int(balance_sheet["annualReports"][10]["totalAssets"]))) * 100
 
+
+def one_yr_liabilities_growth(balance_sheet : dict):
+    """Calculates the 1yr liabilities growth of a company whose balance sheet is 
+    passed as a dictionary-type argument. Returns False if it fails to calculate"""
+
+    check_api_limit_reached(balance_sheet)
+
+    if len(balance_sheet["annualReports"]) < 2 :
+        return False
+
+    if balance_sheet["annualReports"][1]["totalLiabilities"] == "None" or balance_sheet["annualReports"][2]["totalLiabilities"] == "None" or balance_sheet["annualReports"][2]["totalLiabilities"] == "0":
+        return False
+    
+    return ((int(balance_sheet["annualReports"][1]["totalLiabilities"]) - int(balance_sheet["annualReports"][2]["totalLiabilities"]))/(int(balance_sheet["annualReports"][2]["totalLiabilities"]))) * 100
+
+
+def two_yr_liabilities_growth(balance_sheet : dict):
+    """Calculates the 2yr liabilities growth of a company whose balance sheet is 
+    passed as a dictionary-type argument. Returns False if it fails to calculate"""
+
+    check_api_limit_reached(balance_sheet)
+
+    if len(balance_sheet["annualReports"]) < 3 :
+        return False
+
+    if balance_sheet["annualReports"][1]["totalLiabilities"] == "None" or balance_sheet["annualReports"][3]["totalLiabilities"] == "None" or balance_sheet["annualReports"][3]["totalLiabilities"] == "0":
+        return False
+    
+    return ((int(balance_sheet["annualReports"][1]["totalLiabilities"]) - int(balance_sheet["annualReports"][3]["totalLiabilities"]))/(int(balance_sheet["annualReports"][3]["totalLiabilities"]))) * 100
+
+
+def five_yr_liabilities_growth(balance_sheet : dict):
+    """Calculates the 2yr liabilities growth of a company whose balance sheet is 
+    passed as a dictionary-type argument. Returns False if it fails to calculate"""
+
+    check_api_limit_reached(balance_sheet)
+
+    if len(balance_sheet["annualReports"]) < 6 :
+        return False
+
+    if balance_sheet["annualReports"][1]["totalLiabilities"] == "None" or balance_sheet["annualReports"][6]["totalLiabilities"] == "None" or balance_sheet["annualReports"][6]["totalLiabilities"] == "0":
+        return False
+    
+    return ((int(balance_sheet["annualReports"][1]["totalLiabilities"]) - int(balance_sheet["annualReports"][6]["totalLiabilities"]))/(int(balance_sheet["annualReports"][6]["totalLiabilities"]))) * 100
+
+
 def ten_yr_liabilities_growth(balance_sheet : dict):
     """Calculates the 10yr liabilities growth of a company whose balance sheet is 
     passed as a dictionary-type argument. Returns False if it fails to calculate"""
@@ -176,6 +234,7 @@ def ten_yr_liabilities_growth(balance_sheet : dict):
     
     return ((int(balance_sheet["annualReports"][1]["totalLiabilities"]) - int(balance_sheet["annualReports"][10]["totalLiabilities"]))/(int(balance_sheet["annualReports"][10]["totalLiabilities"]))) * 100
 
+
 def ten_yr_cash_flow_growth(cash_flow_report : dict):
     """Calculates the 10yr liabilities growth of a company whose cash flow report is 
     passed as a dictionary-type argument. Returns False if it fails to calculate"""
@@ -189,6 +248,7 @@ def ten_yr_cash_flow_growth(cash_flow_report : dict):
         return False
     
     return ((int(cash_flow_report["annualReports"][1]["operatingCashflow"]) - int(cash_flow_report["annualReports"][10]["operatingCashflow"]))/(int(cash_flow_report["annualReports"][10]["operatingCashflow"]))) * 100
+
 
 def ten_yr_share_count_growth(balance_sheet : dict):
     """Calculates the 10yr share count growth of a company whose balance sheet is 
